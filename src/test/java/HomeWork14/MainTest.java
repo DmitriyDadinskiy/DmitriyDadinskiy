609f34a37faa144e78d929438604539020229955
DmitriyDadinskiy <84646255+DmitriyDadinskiy@users.noreply.github.com>
2021-07-01 04:00:56 +0300

    first commit

--- src/test/java/HomeWork14/MainTest.java
+++ src/test/java/HomeWork14/MainTest.java
@@ -1,28 +1,40 @@
 package HomeWork14;
 
+import org.junit.Assert;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.junit.runners.Parameterized;
-
-import java.util.Arrays;
-
-import static org.junit.Assert.*;
-@RunWith(Parameterized.class)
+//@RunWith(Parameterized.class)
 
 public class MainTest {
 
     @Test
     public void checkArr() {
-        Arrays.asList(new int [][][]{
-                {{4, 4, 4, 5}, {5}},
-                {{4, 3, 7, 5}, {3, 7, 5}},
-                {{5, 5, 5, 4}, {}},
 
-        });
+
+        int [] array = {1, 2, 4, 4, 5, 6, 7, 8, 1};
+        int [] newArr = {5, 6, 7, 8, 1};
+
+        Assert.assertArrayEquals(newArr, Main.checkArr(array));
+
+        int [] array2 = {1, 2, 4, 4, 5,};
+        int [] newArr2 = {5};
+
+        Assert.assertArrayEquals(newArr2, Main.checkArr(array2));
+
+        int [] array3 = {1, 2, 4, 1, 5,};
+        int [] newArr3 = {1,5};
+
+        Assert.assertArrayEquals(newArr3, Main.checkArr(array3));
     }
-
     @Test
     public void checkArr2(){
+    int [] array = {1,2,1,4};
 
+Assert.assertArrayEquals(new boolean[]{true}, Main.checkArr2(array));
+        int [] array1 = {1,1,1,0};
+
+        Assert.assertArrayEquals(new boolean[]{true}, Main.checkArr2(array1));
+        int [] array2 = {2,4,2,4};
+
+        Assert.assertArrayEquals(new boolean[]{true}, Main.checkArr2(array2));
     }
 }
